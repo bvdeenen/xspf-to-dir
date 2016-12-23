ee2946b2ead71a18b30ad71ddbe3bf720e9b7e01
Bart van Deenen <info@vandeenensupport.com>
2016-12-23 15:08:56 +0100

    handles urls in filenames

--- xspf2dir.py
+++ xspf2dir.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 import sys,os,json,xmltodict,subprocess, shutil
+import urllib, urlparse
 
 def p(f, dest, d="/Users/mjv", mount="mac"):
     with open(f) as fd:                     
         doc =xmltodict.parse(fd.read())
         for i,x in enumerate(doc['playlist']['trackList']['track']):
             f=x['location']
+urllib.url2pathname(urlparse.urlparse('file:///home/user/some%20file.txt').path)
+'/home/user/some file.txt'
+
             n,e = os.path.splitext(f)
             dst = os.path.join(dest, "%03d-%s.mp3" %(i, os.path.basename(n)))
             src = os.path.join(mount, os.path.relpath(f,d))
